0a96d7e6fbe18c03c05dba12515932a5e66410f6
Kamiar596 <yousofikamiar@gmail.com>
2026-01-31 14:44:04 -0500

    Added frontend

---
+++ frontend/IntegratedGreenhouse.js
@@ -0,0 +1,23 @@
+
+async function Greenhouse(file){
+    let x = await fetch(file);
+    let y = await x.text();
+
+    const data = await response.json();
+
+    const dataList = document.getElementById('GreenhouseOptions');
+
+    data.forEach(value => {
+            const p = document.createElement('p');
+            p.textContent = `Temperature: ${value.temp}, Name: ${value.hum}, Color: ${value.soil_val}`;
+            dataList.appendChild(p);
+            const br = document.createElement('br');
+            dataList.appendChild(br);
+        
+
+        });
+
+}
+
+
+setInterval(Greenhouse("/greenhouses/"), 100);
